fdcbf48c2df0796137299b30d35bffe7435d46b7
Georgi <joroz95@gmail.com>
2023-04-13 15:03:00 +0300

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::with('authors')
+            ->whereHas('authors')
+            ->take(10)
+            ->orderBy('id', 'DESC')
+            ->get()
+            ->toArray();
+
+        // return [
+        //     'asa'                   => 1,
+        //     'dsjkahdjsahdkjsahjdsa' => 2,
+        // ];
+
+        return inertia('Books/Index', compact('books'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
